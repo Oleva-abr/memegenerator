0c0b17e663268dbaca70c74c30e95e66e400eeab
Olena <72007221+Oleva-abr@users.noreply.github.com>
2024-04-08 00:09:51 +0300

    finish

--- src/app/generator/generator.component.ts
+++ src/app/generator/generator.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ColorEvent } from 'ngx-color';
 import { ColorChromeModule } from "ngx-color/chrome"
 
 
@@ -16,6 +17,9 @@ export class GeneratorComponent {
   topText: string = '';
   bottomText: string = '';
   fileEvent: any;
+  textColor: string = '#000000';
+  backgroundColor: string = "#f9f9fb"
+
 
   preview(e: any) {
     this.fileEvent = e;
@@ -42,12 +46,40 @@ export class GeneratorComponent {
 
     if (ctx) {
       ctx.clearRect(0, 0, canvas.width, canvas.height)
+      ctx.fillStyle = this.backgroundColor;
+      ctx.fillRect(0, 0, canvas.width, canvas.height)
       this.preview(this.fileEvent)
-      ctx.fillStyle = '#000000'
+      ctx.fillStyle = this.textColor
       ctx.font = '50PX Comic Sans Ms'
       ctx.textAlign = "center"
       ctx?.fillText(this.topText, canvas.width / 2, 100)
       ctx?.fillText(this.bottomText, canvas.width / 2, 750)
     }
+  }
+
+
+
+  canvasTextColor($event: ColorEvent) {
+    this.textColor = $event.color.hex;
+    this.drawText();
+  }
+
+
+  canvasBgColor($event: ColorEvent) {
+    this.backgroundColor = $event.color.hex;
+    this.drawText();
+  }
+
+
+  downloadImg() {
+    let canvas = this.myCanvas.nativeElement;
+
+    let image = canvas.toDataURL("image/png");
+
+    let link = document.createElement("a")
+    link.download = "memeImg.png"
+    link.href = image;
+    link.click();
+
   }
 }
